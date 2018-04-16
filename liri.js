63d26bf269d141ea0405197790febfa38cbc2ced
Hillary Lavin <hilavin@gmail.com>
2018-04-16 18:32:22 -0400

    Add "1" to spotify and twitter variables for differentiation

--- liri.js
+++ liri.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 var keys = require('./keys.js'),
     Spotify = require("node-spotify-api"),
     Twitter = require("twitter"),
-    spotify = new Spotify(keys.spotify),
-    twitter = new Twitter(keys.twitter)
+    spotify1 = new Spotify(keys.spotify),
+    twitter1 = new Twitter(keys.twitter)
 
 
-console.log(spotify);
+console.log(spotify1);
 
