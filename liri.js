8a62b9c55dc860384ed640c5a82ebe3ebd538335
Hillary Lavin <hilavin@gmail.com>
2018-04-16 17:20:14 -0400

    Require twitter and spotify modules in liri.js

--- liri.js
+++ liri.js
@@ -1,4 +1,10 @@
 require("dotenv").config();
+require("node-spotify-api");
+require("twitter");
 var keys = require('./keys.js'),
     spotify = new Spotify(keys.spotify),
     client = new Twitter(keys.twitter)
+
+
+console.log(spotify);
+
