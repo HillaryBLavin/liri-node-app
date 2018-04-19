60b86502903238e429153a198b7382e7482c1fc0
Hillary Lavin <hilavin@gmail.com>
2018-04-19 14:15:58 -0400

    Add code from npm twitter docs for accessing tweet data, customize console.log to loop through tweets and print only the text and created_at values

--- liri.js
+++ liri.js
@@ -1,11 +1,23 @@
+// Read and set any environment variables with the dotenv package, per HW instructions
 require("dotenv").config();
 
-var keys = require('./keys.js'),
-    Spotify = require("node-spotify-api"),
-    Twitter = require("twitter"),
-    spotify1 = new Spotify(keys.spotify),
-    twitter1 = new Twitter(keys.twitter)
+// Variables and constructors
+var keys = require('./keys.js'), // Import keys.js
+    Spotify = require("node-spotify-api"), // Require Spotify API package
+    Twitter = require("twitter"), // Require Twitter package
+    spotifyKeys = new Spotify(keys.spotify), // Spotify key constructor
+    twitterKeys = new Twitter(keys.twitter) // Twitter key constructor
 
 
-console.log(spotify1);
+// Code for accessing Twitter feed, provided in NPM Twitter docs 
+var params = {screen_name: 'HillaryBeCoding'};
+twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+    // console.log(tweets);
+        // For-loop to iterate through tweets and console.log only the tweet itself and timestamp
+        for (var i = 0; i < tweets.length; i++) {
+            console.log("@HillarybeCoding tweeted: " + tweets[i].text + "\nTweet created at: " + tweets[i].created_at);
+        }
+    }
+});
 
